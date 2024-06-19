1210cf872d91966cf81cc87d2f6fade0fab3630a
pascal <optimusprime24071@gmail.com>
2024-06-19 08:55:58 -0400

    Ajout de la section des projets

--- src/scrolltrigger.js
+++ src/scrolltrigger.js
@@ -12,6 +12,43 @@ import { TextPlugin } from "gsap/TextPlugin";
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
+let containers = gsap.utils.toArray(".container2");
+
+// we'll create a ScrollTrigger for each panel just to track when each panel's top hits the top of the viewport (we only need this for snapping)
+let tops = container.map(panel => ScrollTrigger.create({trigger: panel, containers: "top top"}));
+
+// gsap.utils.toArray(".container2").forEach((container2, i) =>{
+//   ScrollTrigger.create({
+//     trigger: ".container2",
+//     start: "top bottom",
+//     pin: true,
+//     pinSpacing: false
+//   });
+// });
+
+
+
+
+containers.forEach((container2, i) => {
+  ScrollTrigger.create({
+    trigger: container2,
+    start: () => container2.offsetHeight < window.innerHeight ? "top bottom" : "top top", // if it's shorter than the viewport, we prefer to pin it at the top
+    pin: i === containers.length -1 ? false : true, 
+    pinSpacing: false 
+  });
+});
+
+ScrollTrigger.create({
+  snap: {
+    snapTo: (progress, self) => {
+      let container2Starts = tops.map(st => st.start), // an Array of all the starting scroll positions. We do this on each scroll to make sure it's totally responsive. Starting positions may change when the user resizes the viewport
+          snapScroll = gsap.utils.snap(container2Starts, self.scroll()); // find the closest one
+      return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll); // snapping requires a progress value, so convert the scroll position into a normalized progress value between 0 and 1
+    },
+    duration: 2
+  }
+});
+
 // let timeln = gsap.timeline({
 //   scrollTrigger:{
 //     trigger: ".container",
@@ -41,39 +78,3 @@ tl
   // .to('container2', { opacity: 0, duration: 2 },
   //     2);
 
-let container = gsap.utils.toArray(".container2");
-
-// we'll create a ScrollTrigger for each panel just to track when each panel's top hits the top of the viewport (we only need this for snapping)
-let tops = container.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
-
-// gsap.utils.toArray(".container2").forEach((container2, i) =>{
-//   ScrollTrigger.create({
-//     trigger: ".container2",
-//     start: "top bottom",
-//     pin: true,
-//     pinSpacing: false
-//   });
-// });
-
-
-
-
-container.forEach((container2, i) => {
-  ScrollTrigger.create({
-    trigger: container2,
-    start: () => container2.offsetHeight < window.innerHeight ? "top top" : "bottom bottom", // if it's shorter than the viewport, we prefer to pin it at the top
-    pin: true, 
-    pinSpacing: false 
-  });
-});
-
-ScrollTrigger.create({
-  snap: {
-    snapTo: (progress, self) => {
-      let container2Starts = tops.map(st => st.start), // an Array of all the starting scroll positions. We do this on each scroll to make sure it's totally responsive. Starting positions may change when the user resizes the viewport
-          snapScroll = gsap.utils.snap(container2Starts, self.scroll()); // find the closest one
-      return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll); // snapping requires a progress value, so convert the scroll position into a normalized progress value between 0 and 1
-    },
-    duration: 0.5
-  }
-});
